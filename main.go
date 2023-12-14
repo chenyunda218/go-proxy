e21dd59b4b244cf8130a58ee887ffb7c104861b7
Dan Chen <dan.chen@boardware.com>
2023-12-14 15:39:51 +0800

    feat

--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
-	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,12 +12,9 @@ func main() {
 	router.POST("/woocommerce-api/:notifyUrl/:uri", func(c *gin.Context) {
 		notifyUrl := c.Param("notifyUrl")
 		uri := c.Param("uri")
-		var body map[string]interface{}
-		c.ShouldBindJSON(&body)
 		client := &http.Client{}
 		url := fmt.Sprintf("https://%s/?wc-api=%s", notifyUrl, uri)
-		jsonStr, _ := json.Marshal(&body)
-		req, _ := http.NewRequest(http.MethodPost, url, strings.NewReader(string(jsonStr)))
+		req, _ := http.NewRequest(http.MethodPost, url, c.Request.Body)
 		resp, err := client.Do(req)
 		if err != nil || resp.StatusCode == 404 {
 			c.String(404, "error")
