388a2ec3eb8ac7141a7fc8a1b2f3a6b887463921
Dan Chen <dan.chen@boardware.com>
2023-12-14 15:44:38 +0800

    feat

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 		uri := c.Param("uri")
 		client := &http.Client{}
 		url := fmt.Sprintf("https://%s/?wc-api=%s", notifyUrl, uri)
-		req, _ := http.NewRequest(http.MethodPost, url, c.Request.Body)
+		req, _ := http.NewRequest(http.MethodPost, url, c.Copy().Request.Body)
 		resp, err := client.Do(req)
 		if err != nil || resp.StatusCode == 404 {
 			c.String(404, "error")
